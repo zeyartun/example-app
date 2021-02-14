d16311cacc2003d79761810591df3ae823eb8b38
zeyar <zeyarking125@gmail.com>
2021-02-14 09:12:52 +0630

    local

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'email' => "zeyar@gmail.com",
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'role' => $role[rand(0,1)],
+            'role' => 'Admin',
             'remember_token' => Str::random(10),
         ];
     }
